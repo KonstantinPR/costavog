e7902b7e866a8f109d70634b38fbfef53ec71fa6
Konstantin <konstantinpr@ya.ru>
2022-03-03 22:12:47 +0300

    read and write to bd

--- FDataBase.py
+++ FDataBase.py
@@ -1,3 +1,8 @@
+import sqlite3
+import time
+import math
+
+
 class FDataBase:
     def __init__(self, db):
         self.__db = db
@@ -12,3 +17,25 @@ class FDataBase:
         except:
             print("Ошибка чтения из БД")
         return []
+
+    def addPost(self, title, text):
+        try:
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?)", (title, text, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Ошибка добавления статьи в БД " + str(e))
+            return False
+
+        return True
+
+    def getPost(self, postId):
+        try:
+            self.__cur.execute(f"SELECT title, text FROM posts WHERE id = {postId} LIMIT 1")
+            res = self.__cur.fetchone()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print("Ошибка получения статьи из БД " + str(e))
+
+        return (False, False)
