b636680e55ff7f8a9b080eaf66549321ca306538
Konstantin <konstantinpr@ya.ru>
2022-05-29 01:49:16 +0300

    parameters cropper setting

--- app/modules/img_cropper.py
+++ app/modules/img_cropper.py
@@ -6,12 +6,17 @@ import zipfile
 
 K_HEIGHT_LEFT_START = 0.10
 K_HEIGHT_RIGHT_START = 0.90
-K_BOTTOM_LEFT_START = 0.05
+K_BOTTOM_LEFT_START = 0.10
 K_BOTTOM_RIGHT_START = 0.95
-STEP_ITERATION = 10
+STEP_ITERATION = 25
+STEP_ITERATION_BOTTOM = 10
 SENSIBILITY_COLOR = 210
 COUNT_STEP_J = 10
 DELIMITER = 4
+NEW_HEIGHT_IM = 1600
+K_WIDHT_HEIGHT_IM = 0.75
+WHITE_BLOCK_WIDTH = int(NEW_HEIGHT_IM * K_WIDHT_HEIGHT_IM)
+WHITE_BLOCK_HEIGHT = NEW_HEIGHT_IM
 
 
 def crop_images(images):
@@ -35,18 +40,18 @@ def _crop_img(img):
     fixed_image = ImageOps.exif_transpose(original_image)
     img = fixed_image
     pix = np.array(img)
-    img__crop_height_top = _crop_height(pix, STEP_ITERATION)
-    img__crop_height_bottom, left_border, right_border = _crop_bottom(pix, img__crop_height_top)
-    area = (left_border, img__crop_height_top, right_border, img__crop_height_bottom)  # left, top, right, bottom
+    img_crop_height_top = _crop_height(pix, STEP_ITERATION)
+    img_crop_height_bottom, left_border, right_border = _crop_bottom(pix, img_crop_height_top)
+    area = (left_border, img_crop_height_top, right_border, img_crop_height_bottom)  # left, top, right, bottom
     cropped_img = ImageOps.crop(img, area)
     im = cropped_img
 
     # Унифицировать разрешение по высоте до
 
     old_width_im, old_height_im = im.size
-    new_height_im = 2950
-    k_widht_height_im = 0.75
-    new_width_im = int(new_height_im * k_widht_height_im)
+    new_height_im = NEW_HEIGHT_IM
+    k_width_height_im = K_WIDHT_HEIGHT_IM
+    new_width_im = int(new_height_im * k_width_height_im)
     if old_height_im < old_width_im:
         new_height_im = int(old_height_im * (new_width_im) / old_width_im)
 
@@ -56,7 +61,8 @@ def _crop_img(img):
     print("new_height_im= " + str(new_height_im))
     print("new_width_im= " + str(new_width_im))
 
-    white_pic_size = (2250, 3000)
+    white_pic_size = (WHITE_BLOCK_WIDTH, WHITE_BLOCK_HEIGHT)
+    print("white_pic_size " + str(white_pic_size))
     new_im = Image.new("RGB", white_pic_size, (255, 255, 255, 255))
 
     if old_height_im < old_width_im:
@@ -95,26 +101,25 @@ def _crop_height(img, STEP_ITERATION):
     return new_height
 
 
-def _crop_bottom(img, img__crop_height_top):
+def _crop_bottom(img, img_crop_height_top):
     height, width, color = img.shape
-    step = STEP_ITERATION
+    step = STEP_ITERATION_BOTTOM
     left_start = width * K_BOTTOM_LEFT_START
     right_end = width * K_BOTTOM_RIGHT_START
     new_bottom = 0
     count_i = step * 10
     left_right_border = []
-    for i in img[img__crop_height_top + step * 10::step]:
+    for i in img[img_crop_height_top + step * 10::step]:
         count_j = step * COUNT_STEP_J
         count_row = 0
         for j in i:
             if left_start < count_j < right_end:
                 if sum(j) > SENSIBILITY_COLOR * 3:
-
                     count_row += 1
                     if count_row >= (
                             width * K_BOTTOM_RIGHT_START - width * K_BOTTOM_LEFT_START) - 5 and count_i > height / DELIMITER:
                         print(new_bottom)
-                        new_bottom = height - img__crop_height_top - count_i - step
+                        new_bottom = height - img_crop_height_top - count_i - step
                         break
                 else:
                     left_right_border.append(count_j)
@@ -123,7 +128,7 @@ def _crop_bottom(img, img__crop_height_top):
                 break
         count_i += step
     left_border = min(left_right_border) - step * 12
-    right_border = width - (max(left_right_border) + step * 12)
+    right_border = width - (max(left_right_border) + step * 2)
     print("new bottom " + str(new_bottom))
     print("left_border " + str(left_border))
     print("right_border" + str(right_border))
