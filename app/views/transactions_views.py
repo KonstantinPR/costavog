4b7177bfc6fc165c45f1e4731cc5f3e1d00fde12
Costa <konstantinpr@ya.ru>
2022-06-09 01:43:42 +0300

    show transaction via tmp folders

--- app/views/transactions_views.py
+++ app/views/transactions_views.py
@@ -15,6 +15,8 @@ from flask import url_for
 import os
 import shutil
 from app.modules import transaction_worker
+import urllib.request
+import requests
 
 
 # ///TRANSACTIONS////////////
@@ -241,9 +243,30 @@ def show_yandex_transaction_files(transaction_id):
     transaction = Transaction.query.filter_by(id=transaction_id).first()
     files = y.listdir(transaction.yandex_link)
     images = []
+    id_folder = randrange(1000000000000)
+    static_path = 'app/static/'
+    tmp_img = f'tmp_img_{id_folder}/'
+    tmp_folder = f'{static_path}{tmp_img}'
+    files_folder = f"{tmp_folder}"
+    if not os.path.exists(files_folder):
+        os.makedirs(files_folder)
     for file in files:
-        print(file)
         img = file.file
-        images.append(img)
+        string_start = '&filename='
+        string_end = '.jpg'
+        name_start = img.find(string_start)
+        print(name_start)
+        name_end = img.lower().find('.jpg')
+        print(name_end)
+        name_img = img[name_start + len(string_start):name_end + len(string_end)]
+        print(name_img)
+        url = img
+        images.append(f"{tmp_img}{name_img}")
+        print(f"{tmp_folder}{name_img}")
+        with requests.get(url, stream=True) as r:
+            with open(f"{files_folder}{name_img}", "wb") as f:
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
 
     return render_template('transactions_files_div.html', images=images)
