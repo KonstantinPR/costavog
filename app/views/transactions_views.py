7b924d56541a038c0d9131d7fc66e953b4fd588a
Costa <konstantinpr@ya.ru>
2022-06-08 22:50:57 +0300

    bug fix

--- app/views/transactions_views.py
+++ app/views/transactions_views.py
@@ -242,6 +242,13 @@ def show_yandex_transaction_files(transaction_id):
     files = y.listdir(transaction.yandex_link)
     images = []
     for file in files:
-        images.append(file.file)
+        img = file.file
+        start = img.lower().find("&filename")
+        end = img.lower().find(".jpg")
+        print(start)
+        print(end)
+        new_string_img = img[:start] + img[end + 4:] + img[start:end + 4]
+        print(new_string_img)
+        images.append(new_string_img)
 
     return render_template('transactions_files_div.html', images=images)
