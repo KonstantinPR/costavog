01e1d68e14d54ebdfdb0952ec65a58bc2a75fbea
Costa <konstantinpr@ya.ru>
2022-06-28 20:43:48 +0300

    name photo multiplier function added fix

--- app/modules/text_handler.py
+++ app/modules/text_handler.py
@@ -8,12 +8,15 @@ from app.modules.io_output import io_output
 import numpy as np
 
 
-def names_multiply(df, multiply_number):
-    multiply_number = multiply_number + 1
+def names_multiply(df, multiply):
+    multiply_number = multiply + 1
     list_of_multy = []
     for art in df['Артикул']:
         for n in range(1, multiply_number):
-            art_multy = f'{art}-{n}'
+            art_multy = f'{art}-{n}.JPG'
+            if art_multy in list_of_multy:
+                n = n + multiply
+                art_multy = f'{art}-{n}.JPG'
             list_of_multy.append(art_multy)
     df = pd.DataFrame(list_of_multy)
     return df
