30b7794eb3bd8b4a2eaafc2240d863f55ae0d109
Costa <konstantinpr@ya.ru>
2022-06-28 20:21:01 +0300

    name photo multiplier function added

--- app/modules/io_output.py
+++ app/modules/io_output.py
@@ -12,6 +12,15 @@ def io_output(df: pd.DataFrame) -> BytesIO:
     return output
 
 
+def io_output(df: pd.DataFrame) -> BytesIO:
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer)
+    writer.close()
+    output.seek(0)
+    return output
+
+
 def io_img_output(img: Image.Image) -> BytesIO:
     img_io = BytesIO()
     img.save(img_io, 'JPEG', quality=100)
@@ -19,7 +28,7 @@ def io_img_output(img: Image.Image) -> BytesIO:
     return img_io
 
 
-def io_output_all(file):
-    file_io = BytesIO()
-    file_io.seek(0)
-    return file_io
+# def io_output_all(file_io):
+#     file_io = BytesIO()
+#     file_io.seek(0)
+#     return file_io
--- app/views/catalog_views.py
+++ app/views/catalog_views.py
@@ -1,8 +1,12 @@
 # /// CATALOG ////////////
 import flask
+from werkzeug.datastructures import FileStorage
+from io import BytesIO
 from app import app
 from flask import flash, render_template, request, redirect, send_file
 import pandas as pd
+from app.modules.io_output import io_output
+import numpy as np
 
 
 @app.route('/catalog', methods=['GET', 'POST'])
@@ -19,3 +23,48 @@ def catalog():
             df_input_order.to_html(classes='table table-bordered', header="true", index=False)])
 
     return render_template("upload_catalog.html")
+
+
+@app.route('/image_name_multiply', methods=['GET', 'POST'])
+def image_name_multiply():
+    """Обработка файла txt"""
+    if request.method == 'POST':
+        file_txt: FileStorage = request.files['file']
+
+        if not request.files['file']:
+            flash("Не приложен файл")
+            return render_template('upload_txt.html')
+
+        if not request.form['multiply_number']:
+            flash("Сколько фото делать то будем? Поле пустое")
+            return render_template('upload_txt.html')
+        print(request.form['multiply_number'])
+        multiply = int(request.form['multiply_number'])
+        multiply_number = multiply + 1
+
+        df = pd.read_fwf(file_txt)
+        df = df.T.reset_index().set_axis(['Артикул']).T.reset_index(drop=True)
+
+        print(df)
+
+        list_of_multy = []
+        for art in df['Артикул']:
+            for n in range(1, multiply_number):
+                art_multy = f'{art}-{n}'
+                list_of_multy.append(art_multy)
+        df = pd.DataFrame(list_of_multy)
+
+        towrite = BytesIO()
+        df = df.to_csv(header=False, index=False).encode()
+        towrite.write(df)
+        towrite.seek(0)
+        print(towrite)
+
+        return send_file(
+            towrite,
+            as_attachment=True,
+            attachment_filename='art.txt',
+            mimetype='text/csv'
+        )
+
+    return render_template('upload_txt.html')
