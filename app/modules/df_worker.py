1a05dd9ad1a97e97933e7cbef0f2cd1b2ff8dd40
Costa <konstantinpr@ya.ru>
2022-11-04 00:36:41 +0300

    take_off_boxes func adding

--- app/modules/df_worker.py
+++ app/modules/df_worker.py
@@ -12,8 +12,9 @@ from app.modules import io_output
 
 def df_take_off_boxes(df, sep_boxes: str = 'end'):
     print(df)
-    art_col = 'Артикул товара'
-    qt_col = 'Едете'
+    art_size_col = 'Артикул товара'
+    art_col = 'Артикул'
+    qt_col = 'Кол-во'
     sep_col = 'Разметка'
     box_number = 'Номер коробки'
     answer_col = 'Решение'
@@ -21,26 +22,27 @@ def df_take_off_boxes(df, sep_boxes: str = 'end'):
     can_be = 'Можно'
 
     num_box = 1
-    for idx, val in enumerate(df[art_col]):
+    for idx, val in enumerate(df[art_size_col]):
         df.at[idx, box_number] = num_box
         if df.at[idx, sep_col] == sep_boxes:
             num_box += 1
 
-    for art_set in set(df[art_col]):
+    for art_set in set(df[art_size_col]):
         qt = 0
-        for idx, art_val in enumerate(df[art_col]):
-            if df.at[idx, art_col] == art_set:
+        for idx, art_val in enumerate(df[art_size_col]):
+            if df.at[idx, art_size_col] == art_set:
                 qt = qt + 1
                 df.at[idx, already] = qt
 
-    for idx, art_val in enumerate(df[art_col]):
+    for idx, art_val in enumerate(df[art_size_col]):
         box_num = df.at[idx, box_number]
+        ar_val = df.at[idx, art_col]
         if df.at[idx, already] > df.at[idx, can_be]:
             dfx = df.loc[df[box_number] == box_num]
             print(art_val)
-            print(dfx[art_col])
-            print(art_val in dfx[art_col].values)
-            if art_val in dfx[art_col].values:
+            print(dfx[art_size_col])
+            print(art_val in dfx[art_size_col].values)
+            if ar_val in dfx[art_col].values:
                 if not len(set(dfx[art_col])) > 1:
                     print(len(set(dfx[art_col])))
                     print(f'box_num {box_num}')
