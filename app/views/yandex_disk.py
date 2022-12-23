a3b76e7d2010a072c9edbd408e4ee15267ff657c
Costa <konstantinpr@ya.ru>
2022-12-23 17:08:14 +0300

    bug fix

--- app/views/yandex_disk.py
+++ app/views/yandex_disk.py
@@ -21,6 +21,21 @@ import datetime
 URL = app.config['URL']
 
 
+@app.route('/get_stock_wb', methods=['POST', 'GET'])
+@login_required
+def get_stock_wb():
+    """
+    Достает все остатки с WB через API
+    """
+
+    if request.method == 'POST':
+        df_all_cards = API_WB.get_wb_stock_api()
+        df = io_output.io_output(df_all_cards)
+        file_name = f'wb_api_stock_{str(datetime.datetime.now())}.xlsx'
+        return send_file(df, attachment_filename=file_name, as_attachment=True)
+    return render_template('upload_get_info_wb.html', doc_string=get_info_wb.__doc__)
+
+
 @app.route('/get_info_wb', methods=['POST', 'GET'])
 @login_required
 def get_info_wb():
